daccf873e5555530c0169978aff3bd153d324501
sashgorokhov <sashgorokhov@gmail.com>
2017-03-01 10:16:15 +0300

    Fix tests for py2.7

--- tests/test_formatters.py
+++ tests/test_formatters.py
@@ -1,3 +1,5 @@
+import sys
+
 from telegram_handler import formatters
 
 
@@ -7,7 +9,7 @@ def test_markdown_formatter_exception():
     try:
         raise ValueError('test')
     except ValueError as e:
-        string = formatter.formatException((type(e), e, e.__traceback__))
+        string = formatter.formatException(sys.exc_info())
         assert string.startswith('```')
         assert string.endswith('```')
 
@@ -18,6 +20,6 @@ def test_html_formatter_exception():
     try:
         raise ValueError('test')
     except ValueError as e:
-        string = formatter.formatException((type(e), e, e.__traceback__))
+        string = formatter.formatException(sys.exc_info())
         assert string.startswith('<pre>')
         assert string.endswith('</pre>')
