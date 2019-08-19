4ac0e5cbb6ac3c3f81a57be6b3cea49dfd8e8ec4
sashgorokhov <sashgorokhov@gmail.com>
2019-08-19 17:50:47 +0300

    Update version to 2.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.rst') as readme:
     with open('HISTORY.rst') as history:
         long_description = readme.read() + '\n\n' + history.read()
 
-VERSION = '2.1.1'
+VERSION = '2.2'
 
 setup(
     install_requires=['requests'],
